a2ab6ee13cf0dead0c5a057599c35d197bafe62c
Chaiyapruk Parinyanuntakan <chaiyapruk6494@gmail.com>
2023-12-08 14:30:58 +0700

    Lab7_1

---
+++ lab7_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float a  =  20, b = 30, c = 4;
+    float output;
+
+    output = a - b/c;
+    cout << "output: " << output << "\n";
+    return 0;
+}
